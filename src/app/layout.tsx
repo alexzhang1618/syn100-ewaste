c92e28e8ff18f844b41b4d3884e61fa3a903be53
Alex Zhang <alexzhang1618@gmail.com>
2024-05-23 13:26:18 -0700

    Try to fix fonts bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import styles from './page.module.scss';
 import type { Metadata } from 'next';
-import { DM_Sans } from 'next/font/google';
 import Image from 'next/image';
-
-const dmSans = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'E-Waste | Seventh College',
@@ -17,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <div>
         <Image className={styles.background} src="/background.png" alt="Background Graphic" fill />
       </div>
-      <body className={dmSans.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
